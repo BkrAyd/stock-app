9758616a2ee70116cecb579f7d33dc2a91e801cd
Bkr Ayd <baytgm06@gmail.com>
2025-02-21 21:18:29 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppRouter from "./router/AppRouter";
+import { grey, blueGrey } from "@mui/material/colors";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <div className="App">hi</div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: grey["900"],
+      },
+      secondary: {
+        main: blueGrey["900"],
+      },
+    },
+  });
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+        <ToastContainer />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
